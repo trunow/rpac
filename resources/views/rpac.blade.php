2882bd42bf6aec031cea8e69eabebf9a43a8f869
trunow <slava@trunov.me>
2020-02-05 16:45:12 +0300

    -route logout

--- resources/views/rpac.blade.php
+++ resources/views/rpac.blade.php
@@ -24,12 +24,6 @@
 </head>
 <body>
 <div id="rpac"></div>
-<a id="logout-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();" style="display: none;">
-    {{ __('Logout') }}
-</a>
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
 </body>
 </html>
 @endauth
