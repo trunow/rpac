679870d4e6ab42540387ae9f1eb085d37d5baf5e
trunow <slava@trunov.me>
2020-02-12 16:47:27 +0300

    Revert "Let's use Model->BelongsTo relation to check up permissions for relat…"

--- src/Policies/RpPolicy.php
+++ src/Policies/RpPolicy.php
@@ -1,17 +1,11 @@
 <?php
-
 namespace Trunow\Rpac\Policies;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Support\Str;
 
-class RpPolicy
-{
+class RpPolicy {
 
     use HandlesAuthorization;
 
@@ -30,59 +24,23 @@ class RpPolicy
         return new $this->entityName;
     }
 
-    /**
-     * @param User|null $user
-     * @param string $action
-     * @param Model $entity
-     * @return boolean|null
-     */
     protected function beforeCanForUser(?User $user, $action, $entity)
     {
-        foreach ($this->scopes as $scope) {
-
-            // if policy returns smth then return!
-
+        foreach($this->scopes as $scope) {
             $scopeActionReturn = ($scopeForActionMethod = $this->checkScopeForAction($scope, $action))
                 ? $this->$scopeForActionMethod($user, $entity)
                 : null;
-            if (is_bool($scopeActionReturn)) return $scopeActionReturn;
+            if(is_bool($scopeActionReturn)) return $scopeActionReturn;
 
             // TODO [*__summary__] ??? need it config ??? return summary AND/OR for all
             $scopeReturn = ($scopeMethod = $this->checkScope($scope))
                 ? $this->$scopeMethod($user, $action, $entity)
                 : null;
-            if (is_bool($scopeReturn)) return $scopeReturn;
-
-
-            if ($user && $relation = $this->checkModelMethod($entity, $scope)) {
-                // Model has such relation
-
-                if ($entity->getKey()) {
-                    // Instance has relation to User
-                    if ($user->is($entity->$scope)) {
-                        // Just one TRUE is enough
-                        return true;
-                    }
-                } else {
-                    // Collection
-                    $entity::addGlobalScope($scope, function ($query) use ($user, $relation) {
-                        /** @var Builder $query */
-                        $query->where($relation->getForeignKeyName(), '=', $user->getKey());
-                    });
-                }
-            }
+            if(is_bool($scopeReturn)) return $scopeReturn;
         }
-
     }
 
-    /**
-     * Returns method name
-     * @param string $scope
-     * @param $action
-     * @return string|null
-     */
-    private function checkScopeForAction($scope = '', $action)
-    {
+    private function checkScopeForAction($scope = '', $action){
         $methodName = ($action && $scope && is_string($scope)) ? 'scoped' . ucfirst($action) . 'For' . ucfirst(Str::camel($scope)) : null;
 
         return method_exists($this, $methodName)
@@ -90,32 +48,12 @@ class RpPolicy
             : null;
     }
 
-    private function checkScope($scope = '')
-    {
+    private function checkScope($scope = ''){
         $methodName = ($scope && is_string($scope)) ? 'scopeFor' . ucfirst(Str::camel($scope)) : null;
 
         return method_exists($this, $methodName)
             ? $methodName
             : null;
-    }
-
-    /**
-     * @param Model $entity
-     * @param string $scope
-     * @return BelongsTo|null
-     */
-    private function checkModelMethod($entity, $scope)
-    {
-        if (method_exists($entity, $scope)) {
-            if ($entity->getKey()) {
-                /** @var BelongsTo $relation */
-                $relation = $entity->$scope();
-                if (($relation instanceof BelongsTo) && ($entity->$scope instanceof User)) {
-                    return $relation;
-                }
-            }
-        }
-        return null;
     }
 
     /**
@@ -127,7 +65,7 @@ class RpPolicy
      */
     public function __call($action, $parameters)
     {
-        if (!$action) return false;
+        if(!$action) return false;
 
         $user = $parameters[0] ?? auth()->user();
         $entity = $parameters[1] ?? $this->getEntityClass();
@@ -136,7 +74,7 @@ class RpPolicy
 
         $before = $this->beforeCanForUser($user, $action, $entity);
 
-        if (is_bool($before)) return $before;
+        if(is_bool($before)) return $before;
 
         return $user->can($action, $entity);
     }
@@ -147,43 +85,35 @@ class RpPolicy
      *         E X A M P L E S     S C O P E S
      *
      */
-    protected function scopedViewAnyForOwner(?User $user, $entity)
-    {
+    protected function scopedViewAnyForOwner(?User $user, $entity) {
         $entity::addGlobalScope('owner', function ($query) use ($user) {
-            $query->where('user_id', '=', $user->getKey());
+            $query->where('user_id', '=', $user->key());
         });
     }
-
-    protected function scopedViewForOwner(?User $user, $entity)
-    {
-        return $user->getKey() === $entity->user_id;
+    protected function scopedViewForOwner(?User $user, $entity) {
+        return $user->key() === $entity->user_id;
     }
-
     ////
     protected function scopeForSuperUser(?User $user, $action, $entity)
     {
         return $user->isSu();
     }
-
     protected function scopeForGuest(?User $user, $action, $entity)
     {
         return !$user; //(new User)->isGuest($method, $entity);
     }
-
     protected function scopeForTrue(?User $user, $action, $entity)
     {
         return true;
     }
-
     protected function scopeForFalse(?User $user, $action, $entity)
     {
         return false;
     }
-
     protected function scopeForOwner(?User $user, $action, $entity)
     {
         $entity::addGlobalScope('owner', function ($query) use ($user) {
-            $query->where('user_id', '=', $user->getKey());
+            $query->where('user_id', '=', $user->key());
         });
         //::addGlobalScope(new OwnerScope);
     }
